ad834589421c7e14511174bc92ca1b32db8ab8ff
CORP\pagarcia <pablo.garcia-simavilla.external@capgemini.com>
2024-05-13 13:41:24 +0200

    Angular displaying product table

--- Backend/Spring-boot-ecommerce/src/main/java/com/ecommerce/angularspring/Springbootecommerce/dao/ProductRepository.java
+++ Backend/Spring-boot-ecommerce/src/main/java/com/ecommerce/angularspring/Springbootecommerce/dao/ProductRepository.java
@@ -1,9 +1,11 @@
 package com.ecommerce.angularspring.Springbootecommerce.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.ecommerce.angularspring.Springbootecommerce.entity.Product;
 
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long>{
 
 }
