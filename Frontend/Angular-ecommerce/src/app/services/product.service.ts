ee67549c13b346172c2b680dd684c7135566a766
CORP\pagarcia <pablo.garcia-simavilla.external@capgemini.com>
2024-05-15 06:45:17 +0200

    Angular impl getProductCategories in product service

--- Frontend/Angular-ecommerce/src/app/services/product.service.ts
+++ Frontend/Angular-ecommerce/src/app/services/product.service.ts
@@ -2,27 +2,41 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Product } from '../common/product';
 import { Observable, map } from 'rxjs';
+import { ProductCategory } from '../common/product-category';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  private baseUrl = 'http://localhost:8081/api/products'
+  private baseUrl = 'http://localhost:8081/api/products';
+  private categoryUul = 'http://localhost:8081/api/product-category';
   constructor(private httpClient: HttpClient) { }
 
   getProductList(theCategoryId: number): Observable<Product[]> {
     const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`
-    return this.httpClient.get<GetResponse>(searchUrl).pipe(
+    return this.httpClient.get<GetResponseProducts>(searchUrl).pipe(
       map(response => response._embedded.products)
     );
   }
 
+  getProductCategories(): Observable<ProductCategory[]> {
+    return this.httpClient.get<GetResponseProductCategory>(this.categoryUul).pipe(
+      map(response => response._embedded.productCategory)
+    );
+  }
 
 }
 
-interface GetResponse {
+interface GetResponseProducts {
   _embedded: {
     products: Product[];
+  }
+}
+
+
+interface GetResponseProductCategory {
+  _embedded: {
+    productCategory: ProductCategory[];
   }
 }
