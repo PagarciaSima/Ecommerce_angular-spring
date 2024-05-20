51bffcffd9ac4e47f241048011d05b2f46797875
CORP\pagarcia <pablo.garcia-simavilla.external@capgemini.com>
2024-05-20 07:29:11 +0200

    Angular subscribe to  cartService display totals

--- Frontend/Angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ Frontend/Angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, Renderer2, AfterViewChecked  } from '@angular/core';
 import { ProductService } from 'src/app/services/product.service';
 import { Product } from 'src/app/common/product';
 import { ActivatedRoute } from '@angular/router';
+import { CartItem } from 'src/app/common/cart-item';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-product-list',
@@ -22,9 +24,12 @@ export class ProductListComponent implements OnInit, AfterViewChecked {
 
   previousKeyword: string = "";
 
-  constructor(private productService: ProductService,
-              private route: ActivatedRoute,
-              private renderer: Renderer2) { }
+  constructor(
+    private productService: ProductService,
+    private route: ActivatedRoute,
+    private renderer: Renderer2,
+    private cartService: CartService
+  ) { }
 
   ngOnInit() {
     this.route.paramMap.subscribe(() => {
@@ -123,6 +128,8 @@ export class ProductListComponent implements OnInit, AfterViewChecked {
 
   addToCart(theProduct: Product) {
     console.log(`Adding to cart: ${theProduct.name}, ${theProduct.unitPrice}`);
+    const theCartItem = new CartItem(theProduct);
+    this.cartService.addToCart(theCartItem);
   }
   
 }
