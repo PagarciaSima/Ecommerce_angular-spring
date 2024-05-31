3b7d35c877b5939a3a06ece1d9dceaf1b6d3241b
CORP\pagarcia <pablo.garcia-simavillal@capgemini.com>
2024-05-31 15:11:47 +0200

    Angular securing https frontend

--- Frontend/Angular-ecommerce/src/app/config/my-app-config.ts
+++ Frontend/Angular-ecommerce/src/app/config/my-app-config.ts
@@ -3,7 +3,7 @@ export default  {
     oidc: {
         clientId: '0oahcb4svoGg96bNc5d7',
         issuer: 'https://dev-39618659.okta.com/oauth2/default',
-        redirectUri: 'http://localhost:4200/login/callback',
+        redirectUri: 'https://localhost:4200/login/callback',
         scopes: ['openid', 'profile', 'email']
     }
 }
